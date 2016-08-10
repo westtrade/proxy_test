2727ab22c775828e9d08342b0a2e4bf3ad24ae53
Popov Gena <me@westtrade.tk>
2016-08-10 13:51:59 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"strconv"
+	"strings"
+)
+
+func panicWrapper(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initConfig() (string, string, string) {
+
+	target := flag.String("target", "http://habrahabr.ru/", "proxy target")
+	search := flag.String("search", "", "Searched sentence")
+	replace := flag.String("replace", "", "Replace sentence")
+
+	flag.Parse()
+
+	return *target, *search, *replace
+}
+
+func getRequestString(targetLink string) string {
+
+	resp, err := http.Get(targetLink)
+	panicWrapper(err)
+	defer resp.Body.Close()
+	bodyData, err := ioutil.ReadAll(resp.Body)
+	panicWrapper(err)
+
+	result := string(bodyData)
+
+	return result
+}
+
+func main() {
+	var serverPort int = 3001
+	var serverHost string = ""
+	var listenAddress string = serverHost + ":" + strconv.Itoa(serverPort)
+
+	target, search, replace := initConfig()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		outsidePath := "http://" + path.Clean(strings.Replace(target, "http://", "", 1)+r.URL.Path)
+
+		result := getRequestString(outsidePath)
+		result = strings.Replace(result, search, replace, -1)
+
+		fmt.Fprint(w, result)
+
+	})
+
+	fmt.Printf(`
+        Server started %s
+        Target host: %s
+        Seeking sentence: %s
+        Replace for next sentence: %s
+
+    `, listenAddress, target, search, replace)
+
+	err := http.ListenAndServe(listenAddress, nil)
+	panicWrapper(err)
+}
